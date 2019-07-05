a3c3bfe41ba17c756d21474c56aa8b7cceb72796
FrankieWang <francescafranci@outlook.com>
2019-07-05 14:15:49 -0400

    begin namespacing

--- main.js
+++ main.js
@@ -1,21 +1,45 @@
-console.log("linked");
-$(function() {
-	const extractToken = function(hash) {
+const app ={};
+import {
+	CLIENT_ID,
+	AUTHORIZATION_ENDPOINT,
+	RESOURCE_ENDPOINT,
+	LOCATIONS_ENDPOINT
+} from "./scripts/apiInfo.js";
+app.extractToken =function(hash) {
 		const match = hash.match(/access_token=([\w-]+)/);
 		return !!match && match[1];
 	};
-
-	const CLIENT_ID = "jv9jh96u6125utc2bmb2p8inps";
-	const AUTHORIZATION_ENDPOINT = "https://secure.meetup.com/oauth2/authorize";
-	const RESOURCE_ENDPOINT = "https://api.meetup.com/find/upcoming_events";
-
-	const token = extractToken(document.location.hash);
-	if (token) {
+app.checkOauth = function () {
+	app.token = app.extractToken(document.location.hash);
+	if (app.token) {
 		// $("div.authenticated").show();
-        console.log(token);
-        
+		console.log(app.token);
+		$("a.connect").text("logOut");
 		// $("span.token").text(token);
-
+		$.ajax({
+			url: "http://proxy.hackeryou.com",
+			dataType: "json",
+			method: "GET",
+			// beforeSend: function(xhr) {
+			// 	xhr.setRequestHeader("Authorization", "OAuth " + token);
+			// },
+			data: {
+				reqUrl: LOCATIONS_ENDPOINT,
+				params: {
+					// key: apiKey,
+					query: "toronto",
+					access_token: app.token
+				},
+				// proxyHeaders: {
+				//     'Some-Header': 'goes here'
+				// },
+				xmlToJSON: false,
+				useCache: false
+			}
+		}).then(res => {
+			console.log(res);
+			console.log("happy again");
+		});
 		$.ajax({
 			url: "http://proxy.hackeryou.com",
 			dataType: "json",
@@ -27,10 +51,10 @@ $(function() {
 				reqUrl: RESOURCE_ENDPOINT,
 				params: {
 					// key: apiKey,
-					lon: -73.98999786376953,
-                    lat: 40.75,
-                    "access_token":token
-                },
+					// lon: -73.98999786376953,
+					// lat: 40.75,
+					access_token: app.token
+				},
 				// proxyHeaders: {
 				//     'Some-Header': 'goes here'
 				// },
@@ -56,12 +80,33 @@ $(function() {
 			});
 	} else {
 		// $("div.authenticate").show();
-        console.log("need authentication");
-        
+		console.log("need authentication");
+
 		const authUrl =
-			AUTHORIZATION_ENDPOINT + "?client_id=" + CLIENT_ID + "&response_type=token" + "&redirect_uri=" +
+			AUTHORIZATION_ENDPOINT +
+			"?client_id=" +
+			CLIENT_ID +
+			"&response_type=token" +
+			"&redirect_uri=" +
 			window.location;
 
-		$("a.connect").attr("href", authUrl);
+		$("a.connect")
+			.attr("href", authUrl)
+			.text("logIN");
 	}
+}
+$(function() {
+	app.checkOauth();
+	// const extractToken = function(hash) {
+	// 	const match = hash.match(/access_token=([\w-]+)/);
+		
+	// 	return !!match && match[1];
+	// };
+
+	// const CLIENT_ID = "jv9jh96u6125utc2bmb2p8inps";
+	// const AUTHORIZATION_ENDPOINT = "https://secure.meetup.com/oauth2/authorize";
+	// const RESOURCE_ENDPOINT = "https://api.meetup.com/find/upcoming_events";
+	// const LOCATIONS_ENDPOINT ="https://api.meetup.com/find/locations";
+
+	
 });
---
+++ scripts/apiInfo.js
@@ -0,0 +1,4 @@
+export const CLIENT_ID = "jv9jh96u6125utc2bmb2p8inps";
+export const AUTHORIZATION_ENDPOINT = "https://secure.meetup.com/oauth2/authorize";
+export const RESOURCE_ENDPOINT = "https://api.meetup.com/find/upcoming_events";
+export const LOCATIONS_ENDPOINT = "https://api.meetup.com/find/locations";
